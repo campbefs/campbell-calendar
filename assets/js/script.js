172188a4878d682b3da95d695de088c4c33c7a9a
Christopher Campbell <campbefs@gmail.com>
2021-03-08 01:26:16 -0800

    added local storage ability for text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,52 @@
+// Load Tasks from Local Storage
+timeArr = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
+
+for (i=0; i<9; i++) {
+  arrID = timeArr[i];
+  //console.log(arrID);
+
+  $textAreaID = document.getElementById(arrID);
+  $textAreaID.value = localStorage.getItem(arrID);
+};
+
+//data = localStorage.getItem("4PM");
+
+// Save Tasks
+var saveTasks = function(blockID, textData) {
+  localStorage.setItem(blockID, textData);
+};
 
 
-$(".block").on("click", function() {
-  // var text=$(this)
-  //   .text()
-  //   .trim();
+// Replace the textarea value on blur
+$(".block").on("blur", "textarea", function() {
+  var text = $(this)
+    .val()
+    .trim();
+  
+  // insert into textarea
+  myId = $(this).attr("id");
+  console.log('ID ', myId);
+  myId.value = text;
 
-  // var textInput = $("<textarea>")
-  //   .addClass("form-control")
-  //   .val(text);
+  textID = document.getElementById(myId);
 
-  // $(this).replaceWith(textInput);
+});
 
-  // textInput.trigger("focus");
-  console.log('working');
-})
+// save the textarea value to local storage when on click 'save'
+$(".save").on("click", function(text) {
+
+  // Grab the ID of the corresponding block
+  var myClass = $(this).attr("class");
+  myClass = myClass.split(" ")
+  textID = myClass[2];
+
+  // Grab the textarea text
+  textValue = document.getElementById(textID).value;
+
+  console.log(textValue);
+
+  saveTasks(textID, textValue);
+
+});
+
+
