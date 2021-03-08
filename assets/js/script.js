aba96c1b90a5a35c0b25520a8de4fa4be0dda2e4
Christopher Campbell <campbefs@gmail.com>
2021-03-07 23:43:22 -0800

    basic u/i and text fields

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+
+$(".block").on("click", function() {
+  // var text=$(this)
+  //   .text()
+  //   .trim();
+
+  // var textInput = $("<textarea>")
+  //   .addClass("form-control")
+  //   .val(text);
+
+  // $(this).replaceWith(textInput);
+
+  // textInput.trigger("focus");
+  console.log('working');
+})
